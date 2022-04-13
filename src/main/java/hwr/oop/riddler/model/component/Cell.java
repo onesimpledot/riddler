9ebde4aef57cf7f43ccd8acd42c564ca62703fba
Julius Jänchen <julius.jaenchen@check24.de>
2022-04-13 13:15:21 +0200

    improved constructer

--- src/main/java/hwr/oop/riddler/model/component/Cell.java
+++ src/main/java/hwr/oop/riddler/model/component/Cell.java
@@ -7,7 +7,7 @@ public class Cell {
     public int value;
     public ArrayList<Integer> possibles = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
     
-    Cell(int _value) {
-        value = _value;
+    Cell(int value) {
+        this.value = value;
     }
 }
