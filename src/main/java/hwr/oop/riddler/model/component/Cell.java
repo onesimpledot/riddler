dff78295ec7e0dd15f228a36e2321e9049e041eb
Julius Jänchen <julius.jaenchen@check24.de>
2022-04-13 13:34:35 +0200

    updated Cell.possibles

--- src/main/java/hwr/oop/riddler/model/component/Cell.java
+++ src/main/java/hwr/oop/riddler/model/component/Cell.java
@@ -1,11 +1,11 @@
 package hwr.oop.riddler.model.component;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Cell {
     public int value;
-    public ArrayList<Integer> possibles = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    public Set<Integer> possibles = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
     
     Cell(int value) {
         this.value = value;
