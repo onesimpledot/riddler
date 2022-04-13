7471f36d5984e5064d328ab9deb58ddcb8fc0e65
Julius Jänchen <julius.jaenchen@check24.de>
2022-04-13 12:23:55 +0200

    created Cell class

--- src/main/java/hwr/oop/riddler/model/component/Cell.java
+++ src/main/java/hwr/oop/riddler/model/component/Cell.java
@@ -1,4 +1,13 @@
 package hwr.oop.riddler.model.component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Cell {
+    public int value;
+    public ArrayList<Integer> possibles = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    
+    Cell(int _value) {
+        value = _value;
+    }
 }
