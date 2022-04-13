7d4c635044fae95fce2a15a18fc01789828ba9d1
Julius Jänchen <julius.jaenchen@check24.de>
2022-04-13 12:24:19 +0200

    created sudoku class

--- src/main/java/hwr/oop/riddler/model/Sudoku.java
+++ src/main/java/hwr/oop/riddler/model/Sudoku.java
@@ -1,4 +1,14 @@
 package hwr.oop.riddler.model;
 
+import hwr.oop.riddler.model.component.Box;
+import hwr.oop.riddler.model.component.Column;
+import hwr.oop.riddler.model.component.Row;
+
 public class Sudoku {
+    public Row[] rows = new Row[9];
+    public Column[] columns = new Column[9];
+    public Box[] box = new Box[9];
+
+    public Sudoku(int[][] input) {
+    }
 }
