3a8804a3bf2af96b289196c0e70662d3c9906b83
Julius Jänchen <julius.jaenchen@check24.de>
2022-04-15 18:20:53 +0200

    minor improvement

--- src/main/java/hwr/oop/riddler/model/component/CellGroup.java
+++ src/main/java/hwr/oop/riddler/model/component/CellGroup.java
@@ -1,14 +1,13 @@
 package hwr.oop.riddler.model.component;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public abstract class CellGroup {
     protected Set<Cell> cells = new HashSet<>();
 
     protected CellGroup(Cell[] cells) {
-        this.cells.addAll(List.of(cells));
+        this.cells.addAll(Set.of(cells));
     }
 
     public void whittleDownPossibles(){
