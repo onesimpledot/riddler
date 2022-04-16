53f9eaaeef8af605ad1c3e57430aec03c4e69886
Julius Jänchen <julius.jaenchen@check24.de>
2022-04-16 20:29:18 +0200

    updated Cell component

--- src/main/java/hwr/oop/riddler/model/component/Cell.java
+++ src/main/java/hwr/oop/riddler/model/component/Cell.java
@@ -5,9 +5,12 @@ import java.util.Set;
 
 public class Cell {
     public int value;
-    public Set<Integer> possibles = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    public Set<Integer> possibles;
     
     public Cell(int value) {
         this.value = value;
+        if (value == 0) {
+            possibles = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        }
     }
 }
